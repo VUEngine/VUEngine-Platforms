330bec65c0c75e039936674bc2758c97de8a84ca
Jorge Eremiev <jorgech3@gmail.com>
2026-09-21 13:35:47 -0500

    Removed hack in rumble commands.

--- VirtualBoy/headers/Config.h
+++ VirtualBoy/headers/Config.h
@@ -70,8 +70,6 @@
 // RUMBLE
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-#define __RUMBLE_CMD_DELTA						0
-
 #define __RUMBLE_MAX_EFFECTS_IN_CHAIN			8
 #define __RUMBLE_MAX_OVERDRIVE					126
 #define __RUMBLE_CHAIN_EFFECT_0					0x00
@@ -95,10 +93,10 @@
 #define __RUMBLE_CMD_FREQ_160HZ					0x87
 #define __RUMBLE_CMD_FREQ_240HZ					0x88
 #define __RUMBLE_CMD_FREQ_320HZ					0x89
-#define __RUMBLE_CMD_OVERDRIVE					(0xA0 - __RUMBLE_CMD_DELTA)
-#define __RUMBLE_CMD_SUSTAIN_POS				(0xA1 - __RUMBLE_CMD_DELTA)
-#define __RUMBLE_CMD_SUSTAIN_NEG				(0xA2 - __RUMBLE_CMD_DELTA)
-#define __RUMBLE_CMD_BREAK						(0xA3 - __RUMBLE_CMD_DELTA)
+#define __RUMBLE_CMD_OVERDRIVE					0xA0
+#define __RUMBLE_CMD_SUSTAIN_POS				0xA1
+#define __RUMBLE_CMD_SUSTAIN_NEG				0xA2
+#define __RUMBLE_CMD_BREAK						0xA3
 #define __RUMBLE_CMD_WRITE_EFFECT_CHAIN			0xB0
 #define __RUMBLE_CMD_WRITE_EFFECT_LOOPS_CHAIN 	0xB1
 #define __RUMBLE_EFFECT_CHAIN_END				0xFF
